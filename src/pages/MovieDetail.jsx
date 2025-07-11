7d17c53fd0c2b84b81af91a679b1e1cb530e7d31
Aditya Kumar <30adityakumar@gmail.com>
2025-07-11 06:23:11 -0400

    push

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Header from "./Header"; // Assuming you have a header component!
+import Header from "../components/Header";
 
 const TMDB_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const TMDB_IMG = "https://image.tmdb.org/t/p/w500";

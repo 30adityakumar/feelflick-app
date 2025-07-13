f3c9fff27c1d36de2367787cab551c5925ca4da4
Aditya Kumar <30adityakumar@gmail.com>
2025-07-13 09:15:20 -0400

    push

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from '@/app/App.jsx'
 import '../index.css'
 
-<<<<<<< HEAD
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-=======
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
->>>>>>> bf549458ffcb039177041483b74f2cea850625cf

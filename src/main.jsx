e2f1db35c3cd747249db27f0c26ebf4ca5812540
Aditya Kumar <30adityakumar@gmail.com>
2025-11-26 21:49:51 +0000

    main.jsx update

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/app/router'
 import './index.css'
+import AuthInitializer from '@/app/AuthInitializer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthInitializer>
+      <RouterProvider router={router} />
+    </AuthInitializer>
   </React.StrictMode>
 )
